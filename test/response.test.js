b11d74f7fbe6aa5552067c2bfd16f3b6a8fba0e9
Daniel Jhin Yoo <danieljyoo@gmail.com>
2018-07-09 05:07:29 +0000

    initial checkin

---
+++ test/response.test.js
@@ -0,0 +1,25 @@
+var Funcmatic = require('@funcmatic/funcmatic')
+var ResponsePlugin = require('../lib/response')
+
+var handler = Funcmatic.wrap(async (event, { res }) => {
+  return res.json({ hello: "world" })
+})
+
+describe('Request', () => {
+  beforeEach(() => {
+    Funcmatic.clear()
+  })
+  it ('should return a ', async () => {
+    Funcmatic.use(ResponsePlugin, { })
+    var ret = await handler({ }, { })
+    expect(ret).toMatchObject({
+      statusCode: 200,
+      headers: { 
+        'content-type': 'application/json; charset=utf-8'
+      },
+      body: JSON.stringify({ hello: "world" }),
+      isBase64Encoded: false
+    })
+    console.log("RET", ret)
+  })
+}) 
